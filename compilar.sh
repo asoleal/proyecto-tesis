e886656231221c8140313f59d3ad2935ddb3e37e
John Leal <jlealgom@unal.edu.co>
2025-11-06 21:48:50 -0500

    Correcciones Jurados 1

--- compilar.sh
+++ compilar.sh
@@ -1,5 +1,9 @@
-rm *.aux *.bbl *.blg *.bcf *.run.xml *.log *.toc
+#!/bin/bash
+set -e  # detiene si hay error
+rm -f *.aux *.bbl *.blg *.bcf *.run.xml *.log *.toc
+
 pdflatex 0000.tex
 biber 0000
 pdflatex 0000.tex
 pdflatex 0000.tex
+echo "✅ Compilación completa con biber y biblatex"
